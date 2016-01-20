aef1b5f108ac1e79f4de40fd39be28a5495ed82c
Saamari <sampsa_kautto@hotmail.com>
2016-01-20 15:24:17 +0200

    Demo 4 Homework & Exercises

---
+++ Demo 4  Homework/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_4__Homework
+{
+    class Elevator
+    {
+        
+
+
+        private readonly int TopFloor = 5;
+        private readonly int MinFloor = 1;
+        private int floor;
+
+        public int Floor
+        
+        {
+            get
+            {
+                return floor;
+            }
+
+            set
+            {
+                if (value <= TopFloor && value >= MinFloor) floor = value;
+                else
+                {
+                    Console.WriteLine("Not Such Floor");
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ Demo 4  Homework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// do { 
+//kysy kerros, siirrä hii
+namespace Demo_4__Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+            int number = 0;
+            do
+            {
+                Console.WriteLine("Push a Button 1-5");
+               string line = Console.ReadLine();
+                number = int.Parse(line);
+                elevator.Floor = number;
+                Console.WriteLine("Elevator is in floor " + elevator.Floor);
+                
+
+
+            } while (number != -1);
+
+
+        }
+    }
+}
